3f8a018fc3e4c484a4b1da41373d9bd47421c159
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-31 20:30:42 +0000

    fdm

---
+++ Classes/Read.h
@@ -0,0 +1,24 @@
+//
+// Created by Utilizador on 31-Oct-22.
+//
+
+#ifndef SCHEDULE_READ_H
+#define SCHEDULE_READ_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "Class.h"
+#include "UcList.h"
+#include "Uc.h"
+
+using namespace std;
+
+class Read {
+public:
+    void run();
+};
+
+
+#endif //SCHEDULE_READ_H
