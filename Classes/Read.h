c974c81cb03927d1e507dc6cd4555f355a7b1a6f
Lucas Nakajima <nakajima.lucas02@gmail.com>
2022-10-31 20:17:21 +0000

    b

--- Classes/Read.h
+++ Classes/Read.h
@@ -17,12 +17,16 @@ using namespace std;
 
 class Read {
 public:
+<<<<<<< HEAD:Classes/Read.h
 <<<<<<< HEAD:Classes/DataBase.h
     UcList run();
     DataBase();
 =======
     void run();
 >>>>>>> parent of 16d8be1 (Did somepart of the database):Classes/Read.h
+=======
+    void run();
+>>>>>>> parent of 16d8be1 (Did somepart of the database):Classes/DataBase.h
 };
 
 
